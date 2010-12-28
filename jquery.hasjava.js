42844d0371559e1168f4759b67c6aa16b2b577a9
Chris Dawson <xrdawson@gmail.com>
2010-12-28 13:10:01 -0800

    Initial checkin

---
+++ jquery.hasjava.js
@@ -0,0 +1,9 @@
+(function( $ ){
+    $.fn.hasJava = function( func, path ) {
+        // build a DIV offscreen and check for java
+        if( !path ) {
+            // use the path with the .class
+            // path = 
+        }
+    };
+})( jQuery );
