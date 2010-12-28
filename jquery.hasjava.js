45478ffb5790dd2a43f2bde8818510bd3fec681d
Chris Dawson <xrdawson@gmail.com>
2010-12-28 14:34:04 -0800

    Fixed errors in IE

--- jquery.hasjava.js
+++ jquery.hasjava.js
@@ -1,36 +1,43 @@
 (function( $ ){
     var waited = 0;
 
-    function waitAgain( func ) {
-        if( waited < 10 ) {
-            // is the applet there?
-            if( edocument.jqueryHasJava ) {
-                func( true );
-            }
-            else {
-                waited += 1;
-                setTimeout( function() { waitAgain( func ); }, 1000 );
-            }
-        }
-        else {
-            // call the func with an error
+    var waitAgain = function( func ) {
+        if( waited > 10 ) {
+        // call the func with an error                                                                                                                              
             func( false );
         }
+        else {
+            try {
+          // is the applet there?                                                                                                                                   
+                if( document.jqueryHasJava ) {
+                    // great we have it, test to see if we can script it                                                                                                    
+                    if( 'yes' == document.jqueryHasJava.yes() ) {
+                        func( true );
+                    }
+                }
+                else {
+                    waited += 1;
+                }
+            }
+            catch( err ) {
+                waited += 1;
+            }
+          // try again                                                                                                                                              
+            setTimeout( function() { waitAgain( func ); }, 1000 );
+        }
+    };
 
-    }
-
-    $.fn.hasJava = function( func, path ) {
-        // build a DIV offscreen and check for java
+    $.hasJava = function( func, path ) {
+        // build a DIV offscreen and check for java                                                                                                                 
         if( !path ) {
-            // use the path with the .class
-            path = 'https://github.com/xrd/JqueryHasJava/raw/master/class/hasJava.class';
+            // use the path with the .class                                                                                                                         
+            path = 'http://github.com/xrd/JqueryHasJava/raw/master/class/';
         }
 
-        // Create the div
-        $('<div style="position: absolute; top: -999px; left: -999px; width: 10px; height: 10px;">' +
-          '<applet mayscript="1" name="jqueryHasJava" code="' + path + '" width="1" height="1"></applet></div>' );
-
-        // wait for it to load up to ten seconds
-        setTimeout( waitAgain, 1000 );
+        // Create the div                                                                                                                                           
+        $('body').append($('<div style="position: absolute; top: -999px; left: -999px; width: 10px; height: 10px;">' +
+                           '<applet mayscript="1" name="jqueryHasJava" classpath="' + path + '" code="hasJava" width="1" height="1"></applet></div>' ) );
+        // wait for it to load up to ten seconds                                                                                                                    
+        waitAgain( func );
     };
 })( jQuery );
